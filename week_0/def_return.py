4740660cfc7f95c8323b6ff559b3364def8f72d4
farandgenshin-droid <farandgenshin@gmail.com>
2026-07-16 10:39:29 +0700

    Update def_return.py

--- week_0/def_return.py
+++ week_0/def_return.py
@@ -1,10 +1,12 @@
 def main():
     x = int(input("berapa x nya: "))
-    print("pangkat kuadrat x dengan print adalah", falsesquare(x))
+    print("pangkat kuadrat x dengan print adalah", falsesquare(x)) #ketika di print untuk dipanggil lagi dia tidak kemabli(return), sehingga ouputnya none
     print("pangkat kuadrat x dengan return adalah", square(x))
+    falsesquare(x) # di print pada fungsi def falsesquare nya 
+    square(x) # tidak bakal keluar karena belum di print 
 
 def falsesquare(n): 
-    print (n*n) # tetep terhitung, tapi untuk memanggil di def lain bakal none 
+    print ("pada fungsi falsesquare(n), bukan dipanggil di main  (bukan main)", n*n) # tetep terhitung, tapi untuk memanggil di def lain bakal none 
   
 def square(n):
     return n**2 # jadi return kegunaannya(fungsinya) adalah memasukkan x (square(x))  ke  n (square(n)) lalu n akan di kuadrat, 
